9ff8bdcd9304f791ef43000146033203ef1c78f6
alexmelk <alexmelk@yandex.ru>
2022-09-24 12:39:46 +0300

    initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,18 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent implements OnInit {
+  title = 'app';
+  ngOnInit(): void {
+    window.addEventListener("gamepadconnected", (e) => {
+      console.log("Gamepad connected at index %d: %s. %d buttons, %d axes.",
+        e.gamepad.index, e.gamepad.id,
+        e.gamepad.buttons.length, e.gamepad.axes.length);
+    });
+    
+  }
+}
